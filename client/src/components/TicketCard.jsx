138493f7504e1fb9ff462593d1bc9caa62e5ba72
ChamikaRohan <chamika707@gmail.com>
2024-06-18 19:00:04 +0530

    Binded get all ticket API with explore UI

--- client/src/components/TicketCard.jsx
+++ client/src/components/TicketCard.jsx
@@ -6,10 +6,10 @@ export default function TicketCard({ ticket }) {
     <div className="ticket-card">
       <img src={ticket.photo} alt={ticket.eventName} className="ticket-photo" />
       <div className="ticket-details">
-        <h3 className="event-name">{ticket.eventName}</h3>
-        <p className="event-date">{ticket.date}</p>
+        <h3 className="event-name">{ticket.name}</h3>
+        <p className="event-date">{ticket.validity_date}</p>
         <p className="event-location">{ticket.location}</p>
-        <p className="ticket-price">${ticket.price}</p>
+        <p className="ticket-price">Rs.{ticket.price_lkr}</p>
       </div>
     </div>
   );
--- client/src/pages/ExplorePage.jsx
+++ client/src/pages/ExplorePage.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../components/Footer.jsx';
 import TicketCard from '../components/TicketCard';
 import './ExplorePage.css';
 
-const ticket = {
-  photo: 'https://via.placeholder.com/150',
-  eventName: 'Rock Concert',
-  date: 'June 30, 2024',
-  location: 'Madison Square Garden, NY',
-  price: '99.99',
-};
-
 export default function ExplorePage() {
+const apiURL = import.meta.env.VITE_API_BASE_URL;
+
+const [tickets, setTickets] = useState([]);
+
+useEffect(()=>{
+    getTickets();
+},[]);
+
+const getTickets = async() =>{
+    const response = await fetch(`${apiURL}/main-ticket/get-all-main-tickets`);
+    const data = await response.json();
+    setTickets(data);
+}
+
   return (
     <section className="d-flex justify-content-center align-items-center text-center text-white">
       <div className="container" style={{ margin: '20px', maxWidth: '800px' }}>
         <h2 className="fw-bolder display-5" style={{ color: '#f3a42f' }}>Explore Tickets</h2>
         <div className="ticket-container">
-          <TicketCard ticket={ticket} />
-          <TicketCard ticket={ticket} />
-          <TicketCard ticket={ticket} />
-          <TicketCard ticket={ticket} />
+          {tickets.map((ticket)=>(
+            <TicketCard ticket={ticket} />
+          )) }
+           
         </div>
         <Footer />
       </div>
